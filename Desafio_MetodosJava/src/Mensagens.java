85341daa4586936362938ea9120e78337c35c527
H4ncun <luizgoncalves@hotmail.com.br>
2022-01-25 20:07:01 -0300

    commit - main

--- Desafio_MetodosJava/src/Mensagens.java
+++ Desafio_MetodosJava/src/Mensagens.java
@@ -5,14 +5,14 @@ public class Mensagens {
 		
 		if (hora>=6 && hora <= 12){
 			
-			System.out.print("\n Bom Dia!\n");
+			System.out.print("\n Bom Dia! \n");
 		}
 		else if(hora>=12 && hora <= 18){			
-			System.out.print("\n Boa Tarde!\n");
+			System.out.print("\n Boa Tarde! \n");
 			
 		}
 		else{			
-			System.out.print("\n Boa Noite!\n");
+			System.out.print("\n Boa Noite! \n");
 		}
 	}
 }
